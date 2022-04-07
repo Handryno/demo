d287fb2f5d926a80de2d0e533f65fe94494e6d87
Handry Novianto <handry.novianto@binus.ac.id>
2022-04-07 14:51:23 +0700

    message

--- src/main/java/com/example/demo/controller/CarController.java
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -1,11 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-@RequestMapping
+@RequestMapping(value = "/car")
 public class CarController {
-
+        @GetMapping(value = "checkAPI")
+        public String checkAPI(){
+            return "Hello World";
+    }
 }
