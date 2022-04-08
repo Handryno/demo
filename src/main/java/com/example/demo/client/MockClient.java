86eeef09d522a4156d737c3eb7fe1ba72e1d67c3
Handry Novianto <handrynovianto03@gmail.com>
2022-04-08 14:56:19 +0700

    userController

--- src/main/java/com/example/demo/client/MockClient.java
+++ src/main/java/com/example/demo/client/MockClient.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(value="switching-client", url="${mock.url}", configuration = MockConfiguration.class)
 public interface MockClient {
 
-    @PostMapping(value="auth/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value ="auth/login", consumes = MediaType.APPLICATION_JSON_VALUE)
     LinkedTreeMap authentication(@RequestBody SampleRequest req) throws Exception;
 }
