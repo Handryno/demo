ca5b8ef0fff39689f5ed300a8c690800dca83b86
Handry Novianto <handrynovianto03@gmail.com>
2022-04-12 09:36:29 +0700

    message

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import com.example.demo.dto.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +13,27 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Employee employee = new Employee();
+        employee.setFirstName("Handry");
+        employee.setLastName("Novianto");
+        employee.setEmail("handry@gmail.com");
+        employeeRepository.save(employee);
+
+        Employee employee1 = new Employee();
+        employee1.setFirstName("Arles");
+        employee1.setLastName("Octavianus");
+        employee1.setEmail("Arles@gmail.com");
+        employeeRepository.save(employee1);
     }
 }
--- src/main/java/com/example/demo/dto/Employee.java
+++ src/main/java/com/example/demo/dto/Employee.java
@@ -2,15 +2,28 @@ package com.example.demo.dto;
 
 import lombok.*;
 
+import javax.persistence.*;
+
+
 //@Data
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "employess")
 
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "firstName")
     private String firstName;
+
+    @Column(name = "lastName")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
 }
